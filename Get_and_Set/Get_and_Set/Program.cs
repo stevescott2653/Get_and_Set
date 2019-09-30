cf4865e1ae8bd0e3581ec2c7ccb5d1b5147c584f
Steven Scott <stevescott2653@gmail.com>
2019-09-30 18:34:04 -0400

    Add files via upload

---
+++ Get_and_Set/Get_and_Set/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Get_and_Set
+{
+    public class Employee
+    {
+        private string name;
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            set
+            {
+                this.name = value;
+            }
+        }
+
+        class TestEmployee
+        {
+            public static void Main(string[] args)
+            {
+                Employee e1 = new Employee();
+                e1.Name = "John Doe";
+                Console.WriteLine("Employee Name: " + e1.Name);
+            }
+        }
+    }
+}
